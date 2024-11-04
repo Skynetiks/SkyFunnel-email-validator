792a1bf6d6fd0b3f506451b0590550736e826659
amang06 <aman@skynetiks.com>
2024-11-04 14:13:21 +0530

    eco file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,11 +15,11 @@ module.exports = {
 			name: 'skyfunnel-email-validator-worker',
 			script: './node_modules/.bin/tsx',
 			args: "worker.ts",
-			// exec_mode: 'cluster',
+			exec_mode: 'cluster',
 			watch: true,
-			// output: './logs/worker-out.log',
-			// error: './logs/worker-error.log',
-			// instances: 5,
+			output: './logs/worker-out.log',
+			error: './logs/worker-error.log',
+			instances: 5,
 			env_production: {
 				NODE_ENV: 'production',
 				// Add other environment variables here
