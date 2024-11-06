8063e8a2e44452c4d355fb4f44807b54c96c0ac7
amang06 <aman@skynetiks.com>
2024-11-06 11:38:27 +0530

    implemented esp chack

--- db.ts
+++ db.ts
@@ -1,21 +1,32 @@
 import pg from "pg";
-
 import dotenv from "dotenv";
 
 dotenv.config();
 const { Pool } = pg;
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
+let activePool: pg.Pool;
+
+async function getPool() {
+	if (!activePool) {
+		activePool = new Pool({
+			connectionString: process.env.DATABASE_URL,
+		});
+	}
+	// Log pool errors
+	activePool.on("error", (err) => {
+		console.error("Unexpected error on idle client", err);
+		process.exit(-1);
+	});
+	return activePool;
+}
 
 export const query = async (text: string, params: (string | number)[]) => {
 	try {
+		const pool = await getPool();
 		const result = await pool.query(text, params);
 		return result;
 	} catch (error) {
+		console.error("[Query] Error executing query:", error);
 		throw error;
 	}
 };
-
-export default pool;
---
+++ emailValid.d.ts
@@ -0,0 +1,28 @@
+// emailValid.d.ts
+
+declare module "emailvalid" {
+    interface ValidationResult {
+      valid: boolean;
+      reason?: string;
+    }
+  
+    interface EmailValidationOptions {
+      allowFreemail?: boolean;
+    }
+  
+    class EmailValidation {
+      constructor();
+  
+      // Method to set options for email validation
+      setOptions(options: EmailValidationOptions): void;
+  
+      // Method to add an email domain to the blacklist
+      blacklist(email: string): void;
+  
+      // Method to check if an email is valid
+      check(email: string): ValidationResult;
+    }
+  
+    export = EmailValidation;
+  }
+  
