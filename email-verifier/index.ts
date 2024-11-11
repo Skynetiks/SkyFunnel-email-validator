4052877eadd74bf2a21643bebc6907d9bd85f091
amang06 <aman@skynetiks.com>
2024-11-11 19:32:05 +0530

    remove imap verification

--- email-verifier/index.ts
+++ email-verifier/index.ts
@@ -3,38 +3,37 @@ import { sendVerificationEmail } from "./sendMail";
 import { verifyEmailDeliveryStatus } from "./verifyUsingIMAP";
 import { checkMXRecordsAndSMTP } from "./verifyUsingMXAndSMTP";
 
-export async function EmailVerifier(email: string, firstName: string, waitingTime = 500) {
-	if (!email) {
-		throw new Error("Email is required.");
-	}
+export async function EmailVerifier(
+  email: string,
+  firstName: string,
+  waitingTime = 500
+) {
+  if (!email) {
+    throw new Error("Email is required.");
+  }
 
-	const isEspValid = await InvalidEspCheck(email)
-	if(!isEspValid){
-		console.log(`[VerifyEmail] Email: ${email}, isEspValid: ${isEspValid}`);
-		return { isMXVerified: false, isSMTPVerified: false, isEmailDelivered: false };
-	}
+  const isEspValid = await InvalidEspCheck(email);
+  if (!isEspValid) {
+    console.log(`[VerifyEmail] Email: ${email}, isEspValid: ${isEspValid}`);
+    return false;
+  }
 
-	const { isMXVerified, isSMTPVerified } = await checkMXRecordsAndSMTP(email);
+  const { isMXVerified, isSMTPVerified } = await checkMXRecordsAndSMTP(email);
 
-	// console.log(`[VerifyEmail] Email: ${email}, isEmailValid: ${isEmailValid}`);
+  // console.log(`[VerifyEmail] Email: ${email}, isEmailValid: ${isEmailValid}`);
 
-	let isEmailDelivered = false;
+  // let isEmailDelivered = false;
 
-	if (isEspValid && isMXVerified && isSMTPVerified!==false) {
-		await sendVerificationEmail(email, firstName);
+  if (isEspValid && isMXVerified && isSMTPVerified !== false) {
+    // 	await sendVerificationEmail(email, firstName);
 
-		console.log(`[VerifyEmail] Sent verification email to: ${email}`);
+    // 	console.log(`[VerifyEmail] Sent verification email to: ${email}`);
 
-		await new Promise((resolve) => setTimeout(resolve, waitingTime));
+    // 	await new Promise((resolve) => setTimeout(resolve, waitingTime));
 
-		const isEmailUndelivered = await verifyEmailDeliveryStatus("UndeliveredMail", email);
+    // 	const isEmailUndelivered = await verifyEmailDeliveryStatus("UndeliveredMail", email);
 
-		isEmailDelivered = !isEmailUndelivered;
-	}
-
-	return {
-		isMXVerified,
-		isSMTPVerified,
-		isEmailDelivered,
-	};
+    // 	isEmailDelivered = !isEmailUndelivered;
+    return true;
+  }
 }
--- emailVerifier.ts
+++ emailVerifier.ts
@@ -4,12 +4,12 @@ import { EmailValidity } from "./types";
 export async function verifyEmail(email: string) {
 	let status: EmailValidity = "UNVERIFIED";
 
-	const { isMXVerified, isSMTPVerified, isEmailDelivered } = await EmailVerifier(email, "SkyNavigator");
+	const isEmailValid = await EmailVerifier(email, "SkyNavigator");
 	// if (!isMXVerified) status = "INVALID";
 	// else if (!isSMTPVerified && !isEmailDelivered) status = "MXVERIFIED";
 	// else if (!isEmailDelivered) status = "SMTPVERIFIED";
 	// else status = "VALID";
-	if(isEmailDelivered){
+	if(isEmailValid){
 		status = "VALID";
 	} else {
 		status = "INVALID";
