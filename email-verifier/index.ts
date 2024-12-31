ae6cfc3bbf16c39143ec3ab62251b8aab9665a2f
amang06 <aman@skynetiks.com>
2024-12-31 14:05:25 +0530

    email api

--- email-verifier/index.ts
+++ email-verifier/index.ts
@@ -3,6 +3,34 @@ import { blacklistedEspCheck } from "./blacklistedEspCheck";
 import { mxCheck } from "./mxCheck";
 import { blacklistedEmailCheck } from "./blacklistedEmailCheck";
 
+interface Syntax {
+  username: string;
+  domain: string;
+  valid: boolean;
+}
+
+interface Smtp {
+  host_exists: boolean;
+  full_inbox: boolean;
+  catch_all: boolean;
+  deliverable: boolean;
+  disabled: boolean;
+}
+
+interface EmailVerificationResponse {
+  email: string;
+  reachable: string;
+  syntax: Syntax;
+  smtp: Smtp;
+  gravatar: string | null;
+  suggestion: string;
+  disposable: boolean;
+  role_account: boolean;
+  free: boolean;
+  has_mx_records: boolean;
+}
+
+
 export async function EmailVerifier(email: string): Promise<boolean> {
   email = email.trim();
   if (!email) {
@@ -28,5 +56,23 @@ export async function EmailVerifier(email: string): Promise<boolean> {
   if (!isMxValid) {
     return false;
   }
-  return true;
+
+  try {
+    const response = await fetch(`http://64.23.239.176:8080/v1/${email}/verification`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `${process.env.AUTH_TOKEN}`,
+      },
+    });
+    const data:EmailVerificationResponse = await response.json();
+    if(data.reachable === "yes"){
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error('Error verifying email via API:', error);
+    return false
+  }
+
 }
