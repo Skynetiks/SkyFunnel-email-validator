75080c4c65e05cab136dd01aae9ea5f52b42d101
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2025-02-07 14:13:10 +0530

    fix: set email status to "UNKNOWN" for blacklistedEsp and error

--- types.ts
+++ types.ts
@@ -1,1 +1,1 @@
-export type EmailValidity = "UNVERIFIED" | "INVALID" | "MXVERIFIED" | "SMTPVERIFIED" | "CATCHALL" |"VALID";
+export type EmailValidity = "UNVERIFIED" | "INVALID" | "MXVERIFIED" | "SMTPVERIFIED" | "CATCHALL" |"VALID" | "UNKNOWN";
