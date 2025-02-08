2fca7330f4e6f8fb74cf60025f3433c6cd938301
amang06 <aman@skynetiks.com>
2025-02-08 17:49:12 +0530

    logs

--- email-verifier/blacklistedEmailCheck.ts
+++ email-verifier/blacklistedEmailCheck.ts
@@ -2,9 +2,13 @@ import { query } from "../db.js";
 
 export async function blacklistedEmailCheck(email: string) {
   try {
-    const result = await query('SELECT 1 FROM "BlacklistedEmail" where email=$1', [email]);
+    const result = await query(
+      'SELECT 1 FROM "BlacklistedEmail" where email=$1',
+      [email]
+    );
     if (result.rowCount && result.rowCount > 0) {
-        return true;
+      console.log(`Email ${email} is blacklisted`);
+      return true;
     }
   } catch (e) {
     console.error("Error while fetching blacklisted Email");
--- email-verifier/misspelledCheck.ts
+++ email-verifier/misspelledCheck.ts
@@ -2,8 +2,9 @@ import emailMisspelled, { top100 } from "email-misspelled";
 
 export async function misspelledCheck(email: string) {
   const emailChecker = emailMisspelled({ maxMisspelled: 1, domains: top100 });
-  const result2 = emailChecker(email);
-  if (result2.length > 0) {
+  const result = emailChecker(email);
+  if (result.length > 0) {
+    console.log(`Email ${email} is misspelled: ${result[0]}`);
     return true;
   } else {
     false;
